4c765bf0ae59d603f47fdb87aecc82e9f67d6e69
Huijuan Nan <huijuannan.p@gmail.com>
2015-12-30 00:03:00 +0800

    finish the function

--- main.py
+++ main.py
@@ -1,47 +1,54 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 '''
-Usage:
+Use two different method to derive the parameters of following funcction:
+y = w0 + w1 * x1 + w2 * x2 + w3 * x3
+1. Least linear square: w = linRegress(x,y)
+Using QR decomposition of matrix
+
+2. Stochastic Gradient Descent: w = sgd(x,y, alpha = 0.001,iteration=200)
+alpha: learning rate, default is 0.001
+iteration: iteration time, default is 200
+
 '''
 import numpy as np
-data = np.genfromtxt('data.csv',delimiter=',',skiprows=1)
-x1=data[:,0]
-x2=data[:,1]
-x3=data[:,2]
-y=data[:,3]
 
-sum_x1= np.sum(x1)
-sum_x1_square = np.sum(np.square(x1))
+def linRegress(x,y):
+	sampleNum = len(y)
+	X = np.hstack((np.ones((sampleNum,1)), x))	
+	q, r = np.linalg.qr(X)
+	p = np.dot(q.T,y)
+	return np.dot(np.linalg.inv(r),p)
 
-sum_x2= np.sum(x2)
-sum_x2_square = np.sum(np.square(x2))
 
-sum_x3= np.sum(x3)
-sum_x3_square = np.sum(np.square(x3))
+def sgd(x,y, alpha = 0.001,iteration=200):
+	sampleNum = len(y)
+	x = np.hstack((np.ones((sampleNum,1)), x))
+	varNum = x.shape[1]
 
-sum_y = np.sum(y)
+	w = 2*np.random.random((varNum,1)) - 1
 
-sum_x1_x2 = np.sum(x1*x2)
-sum_x1_x3 = np.sum(x1*x3)
-sum_x1_y = np.sum(x1*y)
-sum_x2_x3 = np.sum(x2*x3)
-sum_x2_y = np.sum(x2*y)
-sum_x3_y = np.sum(x3*y)
-n = np.shape(y)[0]
+	for i in xrange(iteration):
+		for j in xrange(sampleNum):
+			y_estimated = np.dot(x[j], w)
+			y_error = (y_estimated - y[j])*np.ones((1,varNum))
+			w_delta = alpha*y_error*x[j]
+			w = w - w_delta.T			
+	return w.squeeze()
 
-cY = np.array([[sum_y, sum_x1_y, sum_x2_y, sum_x3_y]]).T
-cW0 = np.array([[n, sum_x1, sum_x2, sum_x3]]).T
-cW1 = np.array([[sum_x1, sum_x1_square, sum_x1_x2, sum_x1_x3]]).T
-cW2 = np.array([[sum_x2, sum_x1_x2, sum_x2_square, sum_x2_x3]]).T
-cW3 = np.array([[sum_x3, sum_x1_x3, sum_x2_x3, sum_x3_square]]).T
 
-W0 = np.hstack((cY, cW1, cW2, cW3))
-W1 = np.hstack((cW0, cY, cW2, cW3))
-W2 = np.hstack((cW0, cW1, cY, cW3))
-W3 = np.hstack((cW0, cW1, cW2, cY))
-Y = np.hstack((cW0, cW1, cW2, cW3))
+def Main():
+	data = np.genfromtxt('data.csv',delimiter=',',skiprows=1)
+	x = data[:,:3]
+	y = data[:,3]
+	w_lin = linRegress(x,y)
+	w_sgd = sgd(x,y)
+	print 'function: y = w0 + w1 * x1 + w2 * x2 + w3 * x3'
+	print 'Solution with matrix algorithm:'
+	print 'w0={0:03.3f}, w1={1:03.3f}, w2 = {2:03.3f}, w3 = {3:03.3f}'.format(*w_lin)
+	print 'Solution using Stochastic Gradient Descent:'
+	print 'w0={0:03.3f}, w1={1:03.3f}, w2 = {2:03.3f}, w3 = {3:03.3f}'.format(*w_sgd.T)
 
-w0 = np.linalg.det(W0)/np.linalg.det(Y)
-w1 = np.linalg.det(W1)/np.linalg.det(Y)
-w2 = np.linalg.det(W2)/np.linalg.det(Y)
-w3 = np.linalg.det(W3)/np.linalg.det(Y)
+
+if __name__ == "__main__":
+	Main()
